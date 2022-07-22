09999020ca782b77fd8a5c9cfd2f492aad729c7c
Zafrul097 <zafrulislamridoy@gmail.com>
2022-07-23 00:20:39 +0600

    edit some method

--- MyPlanner/app/src/androidTest/java/com/example/myplanner/ExampleInstrumentedTest.java
+++ MyPlanner/app/src/androidTest/java/com/example/myplanner/ExampleInstrumentedTest.java
@@ -22,7 +22,7 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.codegama.todolistapplication", appContext.getPackageName());
+        assertEquals("com.example.myplanner", appContext.getPackageName());
     }
 
 
