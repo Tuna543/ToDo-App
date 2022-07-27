6dfdd89931501c570b5e59644001ec56725ec884
Zafrul Islam <zafrulislamridoy@gmail.com>
2022-07-27 21:42:59 +0600

    Update UnitTest.java

--- app/src/androidTest/java/com/example/myplanner/UnitTest.java
+++ app/src/androidTest/java/com/example/myplanner/UnitTest.java
@@ -29,7 +29,7 @@ import java.io.IOException;
 
 
          /**
-         * on This class we will do some unit test foer our app
+         * on This class we will do some unit test for our app
          */
 
 
