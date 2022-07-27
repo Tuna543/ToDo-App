c44533d192949f722c96c276397547170b33c3c3
Zafrul Islam <zafrulislamridoy@gmail.com>
2022-07-27 21:28:22 +0600

    Update BaseActivity

--- app/src/main/java/com/example/myplanner/activity/BaseActivity.java
+++ app/src/main/java/com/example/myplanner/activity/BaseActivity.java
@@ -6,9 +6,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-
+/**
+ * This is BaseActivity class of MyPlanner application
+ * MainActivity will extend this class
+ * BaseActivity class
+ *
+ */
 
 public class BaseActivity extends AppCompatActivity {
+    
+    /**onCreate
+     *
+     * @param savedInstanceState
+     */
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
